1fae8b6a51d6c29b6b4bc958d25312aed52ef6bf
Allan Carlos Ramalho <allan.c.ramalho@gmail.com>
2018-04-20 17:04:02 -0300

    Leitura QR funcionando na PoC.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,57 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  qrContent: any;
 
+  constructor(public navCtrl: NavController, navParams: NavParams, private qrScanner: QRScanner, public alertCtrl: AlertController) {
+  }
+
+  ionViewWillEnter() {
+    // Optionally request the permission early
+    this.qrScanner.prepare()
+      .then((status: QRScannerStatus) => {
+        if (status.authorized) {
+          // camera permission was granted
+
+          // start scanning
+          let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+            this.qrContent = text;
+            
+            let alert = this.alertCtrl.create({
+              title: 'QR code',
+              subTitle: this.qrContent,
+              buttons: [
+                {
+                  text: 'OK!',
+                  handler: () => {
+                    console.log('Disagree clicked');
+                    this.navCtrl.setRoot(HomePage);
+                  }
+                }]
+            });
+            alert.present();
+
+            this.qrScanner.hide(); // hide camera preview
+            scanSub.unsubscribe(); // stop scanning
+          });
+
+          // show camera preview
+          this.qrScanner.show();
+
+          // wait for user to scan something, then the observable callback will be called
+        } else if (status.denied) {
+          // camera permission was permanently denied
+          // you must use QRScanner.openSettings() method to guide the user to the settings page
+          // then they can grant the permission from there
+        } else {
+          // permission was denied, but not permanently. You can ask for permission again at a later time.
+        }
+      })
+      .catch((e: any) => console.error('Error is', e));
+  }
+
+  ionViewDidLeave() {
+    this.qrScanner.hide(); // hide camera preview
   }
 
 }
